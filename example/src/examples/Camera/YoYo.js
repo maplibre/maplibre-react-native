6370e23a81e621bf0440e777972ae1b4d2beb571
Ian Wagner <ian.wagner@stadiamaps.com>
2023-01-13 03:00:25 +0900

    Fix YoYo example

--- example/src/examples/Camera/YoYo.js
+++ example/src/examples/Camera/YoYo.js
@@ -2,28 +2,9 @@ import React from 'react';
 import MapLibreGL from '@maplibre/maplibre-react-native';
 
 import sheet from '../../styles/sheet';
-import colors from '../../styles/colors';
 import {SF_OFFICE_COORDINATE} from '../../utils';
 import Page from '../common/Page';
 import BaseExamplePropTypes from '../common/BaseExamplePropTypes';
-
-const layerStyles = {
-  water: {
-    fillColor: [
-      'interpolate',
-      ['exponential', 1],
-      ['zoom'],
-      1,
-      colors.secondary.green,
-      8,
-      colors.secondary.orange,
-      10,
-      colors.secondary.red,
-      18,
-      colors.secondary.yellow,
-    ],
-  },
-};
 
 class YoYo extends React.Component {
   static propTypes = {
@@ -52,7 +33,7 @@ class YoYo extends React.Component {
 
   cameraLoop() {
     requestAnimationFrame(async () => {
-      const nextZoomLevel = this.state.zoomLevel === 12 ? 2 : 12;
+      const nextZoomLevel = this.state.zoomLevel === 6 ? 2 : 6;
       this.setState({zoomLevel: nextZoomLevel});
       this.timeout = setTimeout(() => this.cameraLoop(), 2000);
     });
@@ -69,10 +50,6 @@ class YoYo extends React.Component {
             zoomLevel={this.state.zoomLevel}
             centerCoordinate={SF_OFFICE_COORDINATE}
           />
-
-          <MapLibreGL.VectorSource>
-            <MapLibreGL.FillLayer id="water" style={layerStyles.water} />
-          </MapLibreGL.VectorSource>
         </MapLibreGL.MapView>
       </Page>
     );
