343e7accc963053b68c053298f6c68ababf9db07
Ty Rauber <tyrauber@mac.com>
2024-09-10 16:21:13 +0200

    feat: monorepo configuration

--- packages/react-native-app/metro.config.js
+++ packages/react-native-app/metro.config.js
@@ -1,42 +1,9 @@
 const path = require('path');
-
-// eslint-disable-next-line import/order
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-/*
-  See
-
-  https://medium.com/@dushyant_db/how-to-import-files-from-outside-of-root-directory-with-react-native-metro-bundler-18207a348427
-*/
-
-// exclusionList is a function that takes an array of regexes and combines
-// them with the default exclusions to return a single regex.
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const glob = require('glob-to-regexp');
 
-const extraNodeModules = {
-  '@maplibre/maplibre-react-native': path.resolve(__dirname + '/../maps'),
-};
-
-function getBlacklist() {
-  const nodeModuleDirs = [
-    glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
-    glob(`${path.resolve(__dirname, '..')}/docs/*`),
-    glob(`${path.resolve(__dirname, '..')}/e2e/*`),
-    glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/fbjs/*`),
-    glob(
-      `${path.resolve(
-        __dirname,
-      )}/node_modules/*/node_modules/hoist-non-react-statics/*`,
-    ),
-    glob(
-      `${path.resolve(
-        __dirname,
-      )}/node_modules/react-native/node_modules/@babel/*`,
-    ),
-  ];
-  return exclusionList(nodeModuleDirs);
-}
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
 
 /**
  * Metro configuration
@@ -44,18 +11,31 @@ function getBlacklist() {
  *
  * @type {import('metro-config').MetroConfig}
  */
+
+function withMonorepoPaths(config) {
+  // #1 - Watch all files in the monorepo
+  config.watchFolders = [workspaceRoot];
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
+  ];
+
+  // #3 - Force resolving nested modules to the folders below
+  config.resolver.disableHierarchicalLookup = true;
+
+  return config;
+}
+
 const config = {
-  resolver: {
-    blacklistRE: getBlacklist(),
-    extraNodeModules: new Proxy(extraNodeModules, {
-      get: (target, name) => {
-        return name in target
-          ? target[name]
-          : path.join(process.cwd(), `node_modules/${name}`);
+  projectRoot: projectRoot,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
       },
     }),
   },
-  watchFolders: [path.resolve(__dirname, '..')],
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(withMonorepoPaths(getDefaultConfig(projectRoot)), config);
