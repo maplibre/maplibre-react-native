3a2188fba3d0b3c6a20421e6f02a9f7a6a0e924b
Kilian Finger <finger@trafficon.eu>
2024-10-14 18:47:07 +0200

    fix: add @babel/plugin-transform-private-methods for jest

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: [["@babel/plugin-proposal-class-properties", { loose: true }]],
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+  ],
   env: {
     production: {
       plugins: ["transform-remove-console"],
