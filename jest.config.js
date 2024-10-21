0dd1ddaac3c2017b81d88d469519ac6870953fed
Kilian Finger <finger@trafficon.eu>
2024-10-21 17:03:16 +0200

    chore: configure jest to use with ts

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "@testing-library/react-native",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  setupFilesAfterEnv: ["./jest-setup.ts"],
+  setupFiles: ["./__tests__/__mocks__/react-native.mock.js"],
+  modulePathIgnorePatterns: ["__tests__/__mocks__", "fixtures"],
+  collectCoverageFrom: ["javascript/**/*.{ts,tsx,js,jsx}"],
+};
