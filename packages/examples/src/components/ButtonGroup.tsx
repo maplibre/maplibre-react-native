f17a8ac362ea7fcf3bef9f47aa62ae0cea9c594a
Kilian Finger <finger@trafficon.eu>
2024-10-25 10:56:41 +0200

    chore: remove react native elements from examples (#472)
    * refactor: import components from react-native
    
    * chore: setup App with SafeAreaProvider
    
    * chore: use navigation header instead of custom
    
    * chore: replace react-native-elements usages with custom components
    
    * refactor: unify imports
    
    * chore: remove unused dependencies from examples
    
    * fix: update color from brand guide
    
    * refactor: remove duplicate .png type definition
    
    * fix: align .png type definitions
    
    * fix: add React import to ButtonGroup

---
+++ packages/examples/src/components/ButtonGroup.tsx
@@ -0,0 +1,95 @@
+import React, { Fragment } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import colors from "../styles/colors";
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: "row",
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    borderColor: colors.grey,
+  },
+
+  touchable: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  touchableActive: {
+    backgroundColor: colors.blue,
+  },
+  touchableText: {
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  touchableTextActive: {
+    color: "#ffffff",
+  },
+
+  divider: {
+    width: 2,
+    backgroundColor: "#dedede",
+  },
+});
+
+type ButtonGroupProps = {
+  value?: number;
+  options: string[];
+  onPress: (index: number) => void;
+  disabled?: boolean;
+  scrollable?: boolean;
+};
+
+export function ButtonGroup({
+  value,
+  options,
+  onPress,
+  disabled,
+  scrollable,
+}: ButtonGroupProps) {
+  const buttonGroup = (
+    <View style={styles.root}>
+      {options.map((option, index) => (
+        <Fragment key={option}>
+          {index > 0 && <View style={styles.divider} />}
+          <TouchableOpacity
+            style={[
+              styles.touchable,
+              value === index && styles.touchableActive,
+            ]}
+            onPress={() => {
+              onPress(index);
+            }}
+            disabled={disabled}
+          >
+            <Text
+              style={[
+                styles.touchableText,
+                value === index && styles.touchableTextActive,
+              ]}
+            >
+              {option}
+            </Text>
+          </TouchableOpacity>
+        </Fragment>
+      ))}
+    </View>
+  );
+
+  return scrollable ? (
+    <ScrollView horizontal style={{ flexGrow: 0 }}>
+      {buttonGroup}
+    </ScrollView>
+  ) : (
+    buttonGroup
+  );
+}
