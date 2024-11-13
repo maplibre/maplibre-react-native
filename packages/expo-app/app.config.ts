8385c82feceef2cfe2b7511a1551a1e14955404b
Kilian Finger <finger@trafficon.eu>
2024-11-13 09:15:03 +0100

    fix: make app names safe

--- packages/expo-app/app.config.ts
+++ packages/expo-app/app.config.ts
@@ -2,7 +2,7 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "Expo",
+  name: "ML Expo",
   slug: "maplibre-react-native-expo-example",
   version: "1.0.0",
   newArchEnabled: true,
