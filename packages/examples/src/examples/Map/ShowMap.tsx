5be17ff5859d1530cd3d59b86255e802b8685b2a
Kilian Finger <finger@trafficon.eu>
2024-11-23 12:43:01 +0100

    chore: simplify ShowMap example

--- packages/examples/src/examples/Map/ShowMap.tsx
+++ packages/examples/src/examples/Map/ShowMap.tsx
@@ -1,48 +1,13 @@
 import MapLibreGL from "@maplibre/maplibre-react-native";
-import React, { useEffect, useState } from "react";
-import { Alert } from "react-native";
+import React from "react";
 
 import sheet from "../../styles/sheet";
-import { onSortOptions } from "../../utils";
-import TabBarPage from "../common/TabBarPage";
-
-const OPTIONS = Object.keys(MapLibreGL.StyleURL)
-  .map((key) => {
-    return {
-      label: key,
-      data: (MapLibreGL.StyleURL as any)[key], // bad any, because enums
-    };
-  })
-  .sort(onSortOptions);
+import Page from "../common/Page";
 
 export default function ShowMap() {
-  const [styleURL, setStyleURL] = useState(OPTIONS[0].data);
-
-  useEffect(() => {
-    MapLibreGL.locationManager.start();
-
-    return (): void => {
-      MapLibreGL.locationManager.stop();
-    };
-  }, []);
-
   return (
-    <TabBarPage
-      scrollable
-      options={OPTIONS}
-      onOptionPress={(index, data): void => {
-        setStyleURL(data);
-      }}
-    >
-      <MapLibreGL.MapView styleURL={styleURL} style={sheet.matchParent}>
-        <MapLibreGL.Camera followZoomLevel={6} followUserLocation />
-
-        <MapLibreGL.UserLocation
-          onPress={() => {
-            Alert.alert("You pressed on the user location annotation");
-          }}
-        />
-      </MapLibreGL.MapView>
-    </TabBarPage>
+    <Page>
+      <MapLibreGL.MapView style={sheet.matchParent} />
+    </Page>
   );
 }
