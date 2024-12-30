50f0fcb9024a055c5d274cfbc3bbfa2b73541161
Kilian Finger <finger@trafficon.eu>
2024-12-30 13:28:40 +0100

    refactor: split out ios plugin code

--- src/__tests__/plugin/withMapLibre.test.ts
+++ src/__tests__/plugin/withMapLibre.test.ts
@@ -1,5 +1,5 @@
 import * as fixtures from "./__fixtures__/cocoapodFiles";
-import { applyCocoaPodsModifications } from "../../plugin/withMapLibre";
+import { applyCocoaPodsModifications } from "../../plugin/ios";
 
 describe("applyCocoaPodsModifications", () => {
   it("adds blocks to a react native template podfile", () => {
@@ -7,22 +7,26 @@ describe("applyCocoaPodsModifications", () => {
       applyCocoaPodsModifications(fixtures.reactNativeTemplatePodfile),
     ).toMatchSnapshot();
   });
+
   it("adds blocks to a expo prebuild template podfile", () => {
     expect(
       applyCocoaPodsModifications(fixtures.expoTemplatePodfile),
     ).toMatchSnapshot();
   });
+
   it("adds blocks to a expo prebuild template podfile with custom modifications", () => {
     expect(
       applyCocoaPodsModifications(fixtures.customExpoTemplatePodfile),
     ).toMatchSnapshot();
   });
+
   it("fails to add blocks to a bare podfile", () => {
     expect(() =>
       applyCocoaPodsModifications(fixtures.blankTemplatePodfile),
     ).toThrow("Failed to match");
     expect(() => applyCocoaPodsModifications("")).toThrow("Failed to match");
   });
+
   it("does not re add blocks to an applied template podfile", () => {
     const runOnce = applyCocoaPodsModifications(
       fixtures.reactNativeTemplatePodfile,
@@ -30,6 +34,7 @@ describe("applyCocoaPodsModifications", () => {
 
     expect(applyCocoaPodsModifications(runOnce)).toMatch(runOnce);
   });
+
   it("works after revisions to blocks", () => {
     const runOnce = applyCocoaPodsModifications(
       fixtures.expoTemplateWithRevisions,
@@ -37,6 +42,7 @@ describe("applyCocoaPodsModifications", () => {
 
     expect(runOnce).toMatchSnapshot();
   });
+
   // A known issue is that the regex won't work if the template
   // has a pre_install/post_install block commented out, before the `use_react_native` function.
   it("does not work with revisions to blocks after comments", () => {
