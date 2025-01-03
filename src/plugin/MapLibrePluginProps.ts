69928a53cb6d68af14837bb0e691103ba1eee4fc
Kilian Finger <finger@trafficon.eu>
2025-01-03 17:26:42 +0100

    feat: add Android properties to Expo plugin

--- src/plugin/MapLibrePluginProps.ts
+++ src/plugin/MapLibrePluginProps.ts
@@ -1,7 +1,17 @@
+type VersionString =
+  | `${number}.${number}.${number}`
+  | `${number}.${number}.${number}-${string}`;
+
 export type MapLibrePluginProps =
   | {
       android?: {
+        nativeVersion?: VersionString;
+        pluginVersion?: VersionString;
+        turfVersion?: VersionString;
+        okhttpVersion?: VersionString;
+
         locationEngine?: "default" | "google";
+        googlePlayServicesLocationVersion?: VersionString;
       };
     }
   | undefined;
