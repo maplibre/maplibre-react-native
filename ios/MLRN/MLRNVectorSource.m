268cfc8d0d44ab819eaba917c972bec5c5cba587
Kilian Finger <finger@trafficon.eu>
2025-02-25 08:33:48 +0100

    Revert "fix: pass options for pmtiles"
    This reverts commit 25b1e6559816eb631a27741c14e2291ea0472f5b.

--- ios/MLRN/MLRNVectorSource.m
+++ ios/MLRN/MLRNVectorSource.m
@@ -6,12 +6,11 @@
 {
     if (self.url != nil) {
       if([self.url hasPrefix:@"pmtiles://"]) {
-        return [[MLNVectorTileSource alloc] initWithIdentifier:self.id tileURLTemplates:@[self.url] options:[self getOptions]];
+        return [[MLNVectorTileSource alloc] initWithIdentifier:self.id tileURLTemplates:@[self.url] options:nil];
       } else {
         return [[MLNVectorTileSource alloc] initWithIdentifier:self.id configurationURL:[NSURL URLWithString:self.url]];
       }
     }
-    
     return [[MLNVectorTileSource alloc] initWithIdentifier:self.id tileURLTemplates:self.tileUrlTemplates options:[self getOptions]];
 }
 
