184728cbf84e43985d11c7d268ba188e95c79ec0
Kilian Finger <finger@trafficon.eu>
2025-02-26 17:25:57 +0100

    chore: upgrade to react-native@0.76.7 (#687)

--- src/__tests__/__mocks__/react-native.mock.ts
+++ src/__tests__/__mocks__/react-native.mock.ts
@@ -13,4 +13,4 @@ jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter", () => {
   };
 });
 
-jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+jest.mock("react-native/src/private/animated/NativeAnimatedHelper");
