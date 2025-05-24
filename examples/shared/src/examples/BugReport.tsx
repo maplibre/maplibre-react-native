fb6517ca44ba5e13052b8d78f1e101c69497a51d
Kilian Finger <hey@kilianfinger.com>
2025-05-24 16:14:08 +0200

    chore: add example module

--- examples/shared/src/examples/BugReport.tsx
+++ examples/shared/src/examples/BugReport.tsx
@@ -1,11 +1,15 @@
-import { MapView } from "@maplibre/maplibre-react-native";
+import { MapView, multiply } from "@maplibre/maplibre-react-native";
+import { Text } from "react-native";
 
 export function BugReport() {
   return (
-    <MapView style={{ flex: 1 }}>
-      {/*
+    <>
+      <Text>{multiply(3, 7)}</Text>
+      <MapView style={{ flex: 1 }}>
+        {/*
          Reproduce your Bug here
       */}
-    </MapView>
+      </MapView>
+    </>
   );
 }
