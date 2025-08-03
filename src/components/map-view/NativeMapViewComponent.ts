3b64eb519c3ce084c5edf521932097199dc05264
Kilian Finger <hey@kilianfinger.com>
2025-08-03 20:50:03 +0200

    fix: cleanup NativeMapViewComponent

--- src/components/map-view/NativeMapViewComponent.ts
+++ src/components/map-view/NativeMapViewComponent.ts
@@ -4,11 +4,8 @@ import type {
   DirectEventHandler,
   Double,
   Int32,
-  UnsafeObject,
 } from "react-native/Libraries/Types/CodegenTypes";
 import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
-
-import type { UnsafeMixed } from "../../types/codegen/UnsafeMixed";
 
 type RegionPayloadFeature = {
   type: string;
@@ -29,7 +26,7 @@ type RegionPayloadFeature = {
 export interface NativeProps extends ViewProps {
   contentInset?: number[];
 
-  mapStyle?: UnsafeMixed<object>;
+  mapStyle?: string;
   mapStyleUrl?: string;
 
   preferredFramesPerSecond?: Int32;
@@ -60,49 +57,6 @@ export interface NativeProps extends ViewProps {
   onLongPress?: DirectEventHandler<RegionPayloadFeature>;
   onMapChange?: DirectEventHandler<RegionPayloadFeature>;
 
-  // OLD
-
-  // attributionEnabled?: OptionalProp<boolean>;
-  // attributionPosition?: UnsafeMixed<any>;
-  //
-  // logoEnabled?: OptionalProp<boolean>;
-  // logoPosition?: UnsafeMixed<any>;
-  //
-  // compassEnabled?: OptionalProp<boolean>;
-  // compassFadeWhenNorth?: OptionalProp<boolean>;
-  // compassPosition?: UnsafeMixed<any>;
-  // compassViewPosition?: OptionalProp<Int32>;
-  // compassViewMargins?: OptionalProp<Point>;
-  //
-  // scaleBarEnabled?: OptionalProp<boolean>;
-  // scaleBarPosition?: UnsafeMixed<any>;
-  //
-  // zoomEnabled?: OptionalProp<boolean>;
-  // scrollEnabled?: OptionalProp<boolean>;
-  // rotateEnabled?: OptionalProp<boolean>;
-  // pitchEnabled?: OptionalProp<boolean>;
-  //
-  // deselectAnnotationOnTap?: OptionalProp<boolean>;
-  //
-  // requestDisallowInterceptTouchEvent?: OptionalProp<boolean>;
-  //
-  // projection?: OptionalProp<"mercator" | "globe">;
-  //
-  // // Android only
-  // surfaceView?: OptionalProp<boolean>;
-  // scaleBarViewMargins?: UnsafeMixed<any>;
-  // attributionViewMargins?: UnsafeMixed<any>;
-  // attributionViewPosition?: UnsafeMixed<any>;
-  //
-  // // iOS only
-  // compassImage?: OptionalProp<string>;
-  //
-  // onPress?: BubblingEventHandler<OnPressEventType>;
-  // onLongPress?: DirectEventHandler<OnPressEventType>;
-  // onMapChange?: DirectEventHandler<OnMapChangeEventType>;
-  // onCameraChanged?: DirectEventHandler<OnCameraChangedEventType>;
-  //
-  // mapViewImpl?: OptionalProp<string>;
 }
 
 export default codegenNativeComponent<NativeProps>(
