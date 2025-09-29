2023020ed5932f1e7a13dc9798a394a2209289d0
Kacper Golinski <kacper.golinski@gmail.com>
2025-09-29 21:06:45 +0200

    clean up BugReport.tsx

--- examples/shared/src/examples/BugReport.tsx
+++ examples/shared/src/examples/BugReport.tsx
@@ -1,47 +1,11 @@
-import {
-  MapView,
-  Camera,
-  ShapeSource,
-  LineLayer,
-} from "@maplibre/maplibre-react-native";
-import { type FeatureCollection } from "geojson";
+import { MapView } from "@maplibre/maplibre-react-native";
 
 export function BugReport() {
-  const settings = {
-    bounds: {
-      ne: [17.945307467475175, 55.626240168597775],
-      sw: [17.271308406058154, 49.30274838271916],
-    },
-    padding: {
-      paddingTop: 0,
-      paddingBottom: 400,
-    },
-  };
-
-  const geojson = {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        properties: {},
-        geometry: {
-          coordinates: [
-            [17.945307467475175, 55.626240168597775],
-            [17.271308406058154, 49.30274838271916],
-          ],
-          type: "LineString",
-        },
-      },
-    ],
-  } as FeatureCollection;
-
   return (
     <MapView style={{ flex: 1 }}>
-      <Camera defaultSettings={settings} />
-
-      <ShapeSource id="geojson" shape={geojson}>
-        <LineLayer id="line" style={{ lineColor: "red", lineWidth: 4 }} />
-      </ShapeSource>
+      {/*
+         Reproduce your Bug here
+      */}
     </MapView>
   );
 }
