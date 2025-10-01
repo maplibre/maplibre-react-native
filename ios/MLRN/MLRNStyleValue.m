4a95e0504674b7406e774fee4b3cb99a48ff90db
Kilian Finger <hey@kilianfinger.com>
2025-10-01 22:26:13 +0200

    style: format

--- ios/MLRN/MLRNStyleValue.m
+++ ios/MLRN/MLRNStyleValue.m
@@ -114,33 +114,31 @@
   }
 }
 
-- (BOOL)getImageSdf
-{
-    if ([expressionJSON isKindOfClass:[NSDictionary class]]) {
-        id sdf = [expressionJSON valueForKey:@"sdf"];
-        if ([sdf isKindOfClass:[NSNumber class]]) {
-            return [sdf boolValue];
-        } else {
-            return false;
-        }
+- (BOOL)getImageSdf {
+  if ([expressionJSON isKindOfClass:[NSDictionary class]]) {
+    id sdf = [expressionJSON valueForKey:@"sdf"];
+    if ([sdf isKindOfClass:[NSNumber class]]) {
+      return [sdf boolValue];
     } else {
-        return false;
+      return false;
     }
+  } else {
+    return false;
+  }
 }
 
-- (MLNTransition)getTransition
-{
-    if (![expressionJSON isKindOfClass:[NSDictionary class]]) {
-        return MLNTransitionMake(0.f, 0.f);
-    }
-    
-    NSDictionary *config = (NSDictionary *)expressionJSON;
-    double duration = config[@"duration"] != nil ? [config[@"duration"] doubleValue] : 300.0;
-    double delay = config[@"delay"] != nil ? [config[@"delay"] doubleValue] : 0.0;
+- (MLNTransition)getTransition {
+  if (![expressionJSON isKindOfClass:[NSDictionary class]]) {
+    return MLNTransitionMake(0.f, 0.f);
+  }
 
-    const double millisecondsToSeconds = 1.0/1000.0;
-    
-    return MLNTransitionMake(duration * millisecondsToSeconds, delay * millisecondsToSeconds);
+  NSDictionary *config = (NSDictionary *)expressionJSON;
+  double duration = config[@"duration"] != nil ? [config[@"duration"] doubleValue] : 300.0;
+  double delay = config[@"delay"] != nil ? [config[@"delay"] doubleValue] : 0.0;
+
+  const double millisecondsToSeconds = 1.0 / 1000.0;
+
+  return MLNTransitionMake(duration * millisecondsToSeconds, delay * millisecondsToSeconds);
 }
 
 - (NSExpression *)getSphericalPosition {
