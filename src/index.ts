c208bbcfedd6b482cd01fae996bad8ec3967e215
Kilian Finger <hey@kilianfinger.com>
2025-11-02 02:21:57 +0100

    refactor: export from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,86 @@
-export * from "./MapLibreRN";
+export * from "./MLRNModule";
+
+export {
+  type CameraOptions,
+  type CameraEasing,
+  type CameraAnimationOptions,
+  type CameraCenterOptions,
+  type CameraBoundsOptions,
+  type CameraCenterStop,
+  type CameraBoundsStop,
+  type CameraStop,
+  type InitialViewState,
+  type CameraRef,
+  type TrackUserLocation,
+  type CameraProps,
+  Camera,
+} from "./components/camera/Camera";
+
+export {
+  MapView,
+  type MapViewRef,
+  type ViewState,
+  type ViewStateChangeEvent,
+} from "./components/map-view/MapView";
+
+export { Light } from "./components/Light";
+export { PointAnnotation } from "./components/PointAnnotation";
+export type { PointAnnotationRef } from "./components/PointAnnotation";
+export { Annotation } from "./components/Annotation";
+export { Callout } from "./components/Callout";
+export { requestAndroidLocationPermissions } from "./requestAndroidLocationPermissions";
+export {
+  UserLocation,
+  UserLocationRenderMode,
+} from "./components/UserLocation";
+export type { UserLocationRef } from "./components/UserLocation";
+export { VectorSource } from "./components/VectorSource";
+export { ShapeSource } from "./components/ShapeSource";
+export type { ShapeSourceRef } from "./components/ShapeSource";
+export { RasterSource } from "./components/RasterSource";
+export { ImageSource } from "./components/ImageSource";
+export { Images } from "./components/Images";
+export { FillLayer } from "./components/FillLayer";
+export { FillExtrusionLayer } from "./components/FillExtrusionLayer";
+export { HeatmapLayer } from "./components/HeatmapLayer";
+export { LineLayer } from "./components/LineLayer";
+export { CircleLayer } from "./components/CircleLayer";
+export { SymbolLayer } from "./components/SymbolLayer";
+export { RasterLayer } from "./components/RasterLayer";
+export { BackgroundLayer } from "./components/BackgroundLayer";
+export { MarkerView } from "./components/MarkerView";
+
+export {
+  LocationManager,
+  type Location,
+} from "./modules/location/LocationManager";
+export { OfflineManager } from "./modules/offline/OfflineManager";
+export type { OfflinePackError } from "./modules/offline/OfflineManager";
+export type { OfflinePackStatus } from "./modules/offline/OfflinePack";
+export { OfflinePack } from "./modules/offline/OfflinePack";
+export { OfflineCreatePackOptions } from "./modules/offline/OfflineCreatePackOptions";
+export { SnapshotManager } from "./modules/snapshot/SnapshotManager";
+export type { SnapshotInputOptions } from "./modules/snapshot/SnapshotOptions";
+
+export type { Bounds } from "./types/Bounds";
+export type {
+  FillLayerStyle,
+  LineLayerStyle,
+  SymbolLayerStyle,
+  CircleLayerStyle,
+  HeatmapLayerStyle,
+  FillExtrusionLayerStyle,
+  RasterLayerStyle,
+  HillshadeLayerStyle,
+  BackgroundLayerStyle,
+  LightLayerStyle,
+  Expression,
+} from "./types/MapLibreRNStyles";
+export type { MapLibreRNEvent } from "./types/MapLibreRNEvent";
+export type { PressEventWithFeatures } from "./types/PressEvent";
+export type { ViewPadding } from "./types/ViewPadding";
+
+export { Animated } from "./utils/animated/Animated";
+export { Logger, type LogLevel } from "./utils/Logger";
+
+export type { MapLibrePluginProps } from "./plugin/MapLibrePluginProps";
