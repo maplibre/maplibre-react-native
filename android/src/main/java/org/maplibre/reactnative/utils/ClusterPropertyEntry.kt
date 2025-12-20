9bf5da417cf266cb165b354d8876449477054c45
Kilian Finger <hey@kilianfinger.com>
2025-12-20 10:25:58 +0100

    refactor: migrate to Kotlin

---
+++ android/src/main/java/org/maplibre/reactnative/utils/ClusterPropertyEntry.kt
@@ -0,0 +1,5 @@
+package org.maplibre.reactnative.utils
+
+import org.maplibre.android.style.expressions.Expression
+
+class ClusterPropertyEntry(var operator: Expression?, var mapping: Expression?) 
