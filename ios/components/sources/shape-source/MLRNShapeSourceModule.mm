ff0b05c282178c7137d427933b9ac86a58ffb8e1
Kilian Finger <hey@kilianfinger.com>
2025-12-24 00:33:07 +0100

    fix: MLRNShapeSourceModule

--- ios/components/sources/shape-source/MLRNShapeSourceModule.mm
+++ ios/components/sources/shape-source/MLRNShapeSourceModule.mm
@@ -4,8 +4,11 @@
 #import "FilterParser.h"
 #import "MLRNShapeSource.h"
 #import "MLRNShapeSourceComponentView.h"
+#import "MLRNViewModuleUtils.h"
 
 @implementation MLRNShapeSourceModule
+
+@synthesize viewRegistry_DEPRECATED = _viewRegistry_DEPRECATED;
 
 + (NSString *)moduleName {
   return @"MLRNShapeSourceModule";
@@ -16,62 +19,69 @@
   return std::make_shared<facebook::react::NativeShapeSourceModuleSpecJSI>(params);
 }
 
-- (MLRNShapeSource *)getShapeSourceByReactTag:(NSInteger)reactTag {
-  UIView<RCTComponentViewProtocol> *view = [self.bridge.uiManager viewForReactTag:@(reactTag)];
-
-  if ([view isKindOfClass:[MLRNShapeSourceComponentView class]]) {
-    MLRNShapeSourceComponentView *componentView = (MLRNShapeSourceComponentView *)view;
-    return componentView.shapeSource;
-  }
-
-  RCTLogError(@"Invalid react tag, could not find MLRNShapeSource");
-  return nil;
+- (void)withShapeSource:(NSInteger)reactTag
+                  block:(void (^)(MLRNShapeSource *))block
+                 reject:(RCTPromiseRejectBlock)reject
+             methodName:(NSString *)methodName {
+  [MLRNViewModuleUtils withView:self.viewRegistry_DEPRECATED
+                       reactTag:reactTag
+             componentViewClass:[MLRNShapeSourceComponentView class]
+               contentViewClass:[MLRNShapeSource class]
+                          block:^(UIView *view) {
+                            block((MLRNShapeSource *)view);
+                          }
+                         reject:reject
+                     methodName:methodName];
 }
 
 - (void)getData:(NSInteger)reactTag
          filter:(NSArray *)filter
         resolve:(nonnull RCTPromiseResolveBlock)resolve
          reject:(nonnull RCTPromiseRejectBlock)reject {
-  MLRNShapeSource *shapeSource = [self getShapeSourceByReactTag:reactTag];
-  if (shapeSource == nil) {
-    reject(@"invalid_tag", @"Could not find MLRNShapeSource", nil);
-    return;
-  }
+  [self withShapeSource:reactTag
+                  block:^(MLRNShapeSource *shapeSource) {
+                    NSPredicate *predicate = [FilterParser parse:filter];
+                    NSArray<id<MLNFeature>> *shapes =
+                        [shapeSource featuresMatchingPredicate:predicate];
 
-  NSPredicate *predicate = [FilterParser parse:filter];
-  NSArray<id<MLNFeature>> *shapes = [shapeSource featuresMatchingPredicate:predicate];
+                    NSMutableArray<NSDictionary *> *features =
+                        [[NSMutableArray alloc] initWithCapacity:shapes.count];
+                    for (int i = 0; i < shapes.count; i++) {
+                      [features addObject:shapes[i].geoJSONDictionary];
+                    }
 
-  NSMutableArray<NSDictionary *> *features = [[NSMutableArray alloc] initWithCapacity:shapes.count];
-  for (int i = 0; i < shapes.count; i++) {
-    [features addObject:shapes[i].geoJSONDictionary];
-  }
-
-  resolve(@{@"type" : @"FeatureCollection", @"features" : features});
+                    resolve(@{@"type" : @"FeatureCollection", @"features" : features});
+                  }
+                 reject:reject
+             methodName:@"getData"];
 }
 
 - (void)getClusterExpansionZoom:(NSInteger)reactTag
                       clusterId:(NSInteger)clusterId
                         resolve:(nonnull RCTPromiseResolveBlock)resolve
                          reject:(nonnull RCTPromiseRejectBlock)reject {
-  MLRNShapeSource *shapeSource = [self getShapeSourceByReactTag:reactTag];
-  if (shapeSource == nil) {
-    reject(@"invalid_tag", @"Could not find MLRNShapeSource", nil);
-    return;
-  }
+  [self
+      withShapeSource:reactTag
+                block:^(MLRNShapeSource *shapeSource) {
+                  // Convert clusterId to a GeoJSON feature
+                  NSString *featureJSON = [NSString
+                      stringWithFormat:@"{\"type\":\"Feature\",\"properties\":{\"cluster_id\":%"
+                                       @"ld},"
+                                       @"\"geometry\":{\"type\":\"Point\",\"coordinates\":[0,0]}}",
+                                       (long)clusterId];
 
-  // Convert clusterId to a GeoJSON feature
-  NSString *featureJSON =
-      [NSString stringWithFormat:@"{\"type\":\"Feature\",\"properties\":{\"cluster_id\":%ld},"
-                                 @"\"geometry\":{\"type\":\"Point\",\"coordinates\":[0,0]}}",
-                                 (long)clusterId];
-
-  double zoom = [shapeSource getClusterExpansionZoom:featureJSON];
-  if (zoom == -1) {
-    reject(@"zoom_error",
-           [NSString stringWithFormat:@"Could not get zoom for cluster %ld", (long)clusterId], nil);
-    return;
-  }
-  resolve(@(zoom));
+                  double zoom = [shapeSource getClusterExpansionZoom:featureJSON];
+                  if (zoom == -1) {
+                    reject(@"zoom_error",
+                           [NSString stringWithFormat:@"Could not get zoom for cluster %ld",
+                                                      (long)clusterId],
+                           nil);
+                    return;
+                  }
+                  resolve(@(zoom));
+                }
+               reject:reject
+           methodName:@"getClusterExpansionZoom"];
 }
 
 - (void)getClusterLeaves:(NSInteger)reactTag
@@ -80,54 +90,58 @@
                   offset:(NSInteger)offset
                  resolve:(nonnull RCTPromiseResolveBlock)resolve
                   reject:(nonnull RCTPromiseRejectBlock)reject {
-  MLRNShapeSource *shapeSource = [self getShapeSourceByReactTag:reactTag];
-  if (shapeSource == nil) {
-    reject(@"invalid_tag", @"Could not find MLRNShapeSource", nil);
-    return;
-  }
+  [self
+      withShapeSource:reactTag
+                block:^(MLRNShapeSource *shapeSource) {
+                  // Convert clusterId to a GeoJSON feature
+                  NSString *featureJSON = [NSString
+                      stringWithFormat:@"{\"type\":\"Feature\",\"properties\":{\"cluster_id\":%"
+                                       @"ld},"
+                                       @"\"geometry\":{\"type\":\"Point\",\"coordinates\":[0,0]}}",
+                                       (long)clusterId];
 
-  // Convert clusterId to a GeoJSON feature
-  NSString *featureJSON =
-      [NSString stringWithFormat:@"{\"type\":\"Feature\",\"properties\":{\"cluster_id\":%ld},"
-                                 @"\"geometry\":{\"type\":\"Point\",\"coordinates\":[0,0]}}",
-                                 (long)clusterId];
+                  NSArray<id<MLNFeature>> *shapes = [shapeSource getClusterLeaves:featureJSON
+                                                                           number:limit
+                                                                           offset:offset];
 
-  NSArray<id<MLNFeature>> *shapes = [shapeSource getClusterLeaves:featureJSON
-                                                           number:limit
-                                                           offset:offset];
+                  NSMutableArray<NSDictionary *> *features =
+                      [[NSMutableArray alloc] initWithCapacity:shapes.count];
+                  for (int i = 0; i < shapes.count; i++) {
+                    [features addObject:shapes[i].geoJSONDictionary];
+                  }
 
-  NSMutableArray<NSDictionary *> *features = [[NSMutableArray alloc] initWithCapacity:shapes.count];
-  for (int i = 0; i < shapes.count; i++) {
-    [features addObject:shapes[i].geoJSONDictionary];
-  }
-
-  resolve(features);
+                  resolve(features);
+                }
+               reject:reject
+           methodName:@"getClusterLeaves"];
 }
 
 - (void)getClusterChildren:(NSInteger)reactTag
                  clusterId:(NSInteger)clusterId
                    resolve:(nonnull RCTPromiseResolveBlock)resolve
                     reject:(nonnull RCTPromiseRejectBlock)reject {
-  MLRNShapeSource *shapeSource = [self getShapeSourceByReactTag:reactTag];
-  if (shapeSource == nil) {
-    reject(@"invalid_tag", @"Could not find MLRNShapeSource", nil);
-    return;
-  }
+  [self
+      withShapeSource:reactTag
+                block:^(MLRNShapeSource *shapeSource) {
+                  // Convert clusterId to a GeoJSON feature
+                  NSString *featureJSON = [NSString
+                      stringWithFormat:@"{\"type\":\"Feature\",\"properties\":{\"cluster_id\":%"
+                                       @"ld},"
+                                       @"\"geometry\":{\"type\":\"Point\",\"coordinates\":[0,0]}}",
+                                       (long)clusterId];
 
-  // Convert clusterId to a GeoJSON feature
-  NSString *featureJSON =
-      [NSString stringWithFormat:@"{\"type\":\"Feature\",\"properties\":{\"cluster_id\":%ld},"
-                                 @"\"geometry\":{\"type\":\"Point\",\"coordinates\":[0,0]}}",
-                                 (long)clusterId];
+                  NSArray<id<MLNFeature>> *shapes = [shapeSource getClusterChildren:featureJSON];
 
-  NSArray<id<MLNFeature>> *shapes = [shapeSource getClusterChildren:featureJSON];
+                  NSMutableArray<NSDictionary *> *features =
+                      [[NSMutableArray alloc] initWithCapacity:shapes.count];
+                  for (int i = 0; i < shapes.count; i++) {
+                    [features addObject:shapes[i].geoJSONDictionary];
+                  }
 
-  NSMutableArray<NSDictionary *> *features = [[NSMutableArray alloc] initWithCapacity:shapes.count];
-  for (int i = 0; i < shapes.count; i++) {
-    [features addObject:shapes[i].geoJSONDictionary];
-  }
-
-  resolve(features);
+                  resolve(features);
+                }
+               reject:reject
+           methodName:@"getClusterChildren"];
 }
 
 @end
