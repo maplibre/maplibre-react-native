bd00c382ae52efd8a82332dfa0e52bc529b30c3a
Kilian Finger <hey@kilianfinger.com>
2026-02-08 12:47:31 +0100

    fix: migrate UserLocation to new Layer props

--- src/components/user-location/UserLocation.tsx
+++ src/components/user-location/UserLocation.tsx
@@ -1,9 +1,11 @@
 import { memo, type ReactNode, useMemo } from "react";
 
 import { UserLocationPuck } from "./UserLocationPuck";
+import headingIcon from "../../assets/heading.png";
 import { useCurrentPosition } from "../../hooks/useCurrentPosition";
 import type { LngLat } from "../../types/LngLat";
 import { LayerAnnotation } from "../annotations/LayerAnnotation";
+import { Images } from "../images/Images";
 
 export interface UserLocationProps {
   /**
@@ -59,26 +61,34 @@ export const UserLocation = memo(
     }
 
     return (
-      <LayerAnnotation
-        animated={animated}
-        id="mlrn-user-location"
-        testID="mlrn-user-location"
-        onPress={onPress}
-        lngLat={lngLat}
-      >
-        {children || (
-          <UserLocationPuck
-            testID="mlrn-user-location-puck"
-            source="mlrn-user-location"
-            accuracy={accuracy ? currentPosition.coords.accuracy : undefined}
-            heading={
-              heading
-                ? (currentPosition.coords.heading ?? undefined)
-                : undefined
-            }
+      <>
+        {heading && (
+          <Images
+            images={{
+              "mlrn-user-location-puck-heading": headingIcon,
+            }}
           />
         )}
-      </LayerAnnotation>
+
+        <LayerAnnotation
+          animated={animated}
+          id="mlrn-user-location"
+          testID="mlrn-user-location"
+          onPress={onPress}
+          lngLat={lngLat}
+        >
+          {children || (
+            <UserLocationPuck
+              testID="mlrn-user-location-puck"
+              source="mlrn-user-location"
+              accuracy={accuracy ? currentPosition.coords.accuracy : undefined}
+              heading={
+                heading ? (currentPosition.coords.heading ?? 15) : undefined
+              }
+            />
+          )}
+        </LayerAnnotation>
+      </>
     );
   },
 );
