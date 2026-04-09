4786826d1375df07eefb31da5a41c257fc8b9897
Kilian Finger <hey@kilianfinger.com>
2026-04-09 20:58:03 +0200

    fix(Android): keep MapLibre.getInstance from module

--- package/android/src/main/java/org/maplibre/reactnative/components/mapview/MLRNMapViewModule.kt
+++ package/android/src/main/java/org/maplibre/reactnative/components/mapview/MLRNMapViewModule.kt
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.module.annotations.ReactModule
+import org.maplibre.android.MapLibre
 import org.maplibre.reactnative.NativeMapViewModuleSpec
 import org.maplibre.reactnative.utils.ConvertUtils
 import org.maplibre.reactnative.utils.ExpressionParser
@@ -19,6 +20,10 @@ class MLRNMapViewModule(
 ) : NativeMapViewModuleSpec(reactContext) {
     companion object {
         const val NAME = "MLRNMapViewModule"
+    }
+
+    override fun initialize() {
+        reactApplicationContext.runOnUiQueueThread { MapLibre.getInstance(reactApplicationContext) }
     }
 
     private fun withViewportOnUIThread(
